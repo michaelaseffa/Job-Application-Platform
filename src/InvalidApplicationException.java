cd35cc2c3e0d9edff17ce07d10d75c3b474bef18
kebronteshome-hub <kebron.teshome@bitscolllege.edu.et>
2026-06-24 12:41:19 +0300

    Remove unnecessary files

--- src/InvalidApplicationException.java
+++
@@ -1,7 +0,0 @@
-public class InvalidApplicationException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public InvalidApplicationException(String message) {
-        super(message);
-    }
-}
